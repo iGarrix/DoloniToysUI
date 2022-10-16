547bba072030b082bb751acb959e3d3b2c232e39
garrixdit@gmail.com <garrixdit@gmail.com>
2022-10-16 20:41:30 +0300

    Change api url

--- src/Configurations/api.ts
+++ src/Configurations/api.ts
@@ -1,2 +1,3 @@
-export const apiUrl = "https://localhost:7181/";
+//export const apiUrl = "https://localhost:7181/"; // Local Api
+export const apiUrl = "http://smartertest2022-001-site1.dtempurl.com/"; // Delpoy API
 export const defaultErrorMessage = "Service is temporality disabled";
